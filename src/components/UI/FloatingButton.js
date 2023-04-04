d441281cffa72d2bc564588cde2e71b524954582
MdAmine <saoud93@gmail.com>
2023-04-04 10:28:21 +0000

    init project

---
+++ src/components/UI/FloatingButton.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+import "./FloatingButton.scss";
+
+const FloatingButton = () => {
+  const [checked, setChecked] = useState(false);
+
+  const handleClick = () => {
+    setChecked(!checked);
+  };
+
+  const setUnchecked = () => {
+    setChecked(false);
+  };
+
+  return (
+    <div className="buttonContainer" onBlur={setUnchecked}>
+      <input
+        type="checkbox"
+        id="toggle"
+        className={checked ? "checked" : ""}
+        onClick={handleClick}
+      />
+      <label className="button" htmlFor="toggle"></label>
+      <nav className="nav">
+        <ul>
+          <span>Todo List</span>
+          <span>About</span>
+          <span>Logout</span>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+export default FloatingButton;
